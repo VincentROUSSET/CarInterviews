0391677537134867c1cdba58735c2427ef6c3b63
VincentROUSSET <vincent601998@outlook.fr>
2022-05-23 14:01:18 +0200

    Add files via upload

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+export * from './ping.controller';
+export * from './car.controller';
+export * from './user.controller';
+export * from './booklet.controller';
+export * from './interview.controller';
---
+++ src/repositories/index.ts
@@ -0,0 +1,4 @@
+export * from './booklet.repository';
+export * from './car.repository';
+export * from './interview.repository';
+export * from './user.repository';
