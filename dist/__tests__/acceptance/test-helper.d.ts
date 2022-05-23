7c623b3b77aeff16abed20fd1254a50db0d34a48
VincentROUSSET <vincent601998@outlook.fr>
2022-05-23 14:23:00 +0200

    Delete dist directory

--- dist/__tests__/acceptance/test-helper.d.ts
+++
@@ -1,7 +0,0 @@
-import { CarInterviewsApplication } from '../..';
-import { Client } from '@loopback/testlab';
-export declare function setupApplication(): Promise<AppWithClient>;
-export interface AppWithClient {
-    app: CarInterviewsApplication;
-    client: Client;
-}
